c48ad8ea9b17cc56110aa3f316bf28f7291736c9
João Gardenberg <joaogardenberg@gmail.com>
2020-06-18 04:34:34 -0300

    Update

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        print("Hello Johnny");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
+
