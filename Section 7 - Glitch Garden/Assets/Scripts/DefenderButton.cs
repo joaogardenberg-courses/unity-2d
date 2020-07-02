20205c8124e25ab2fccf5513b76d428387550521
João Gardenberg <joaogardenberg@gmail.com>
2020-07-02 02:41:21 -0300

    Update

--- Section 7 - Glitch Garden/Assets/Scripts/DefenderButton.cs
+++ Section 7 - Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -1,9 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour {
     [SerializeField] Defender defender;
+
+    private void Start() {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost() {
+        Text costText = GetComponentInChildren<Text>();
+
+        if (costText) {
+            costText.text = defender.GetStarCost().ToString();
+        } else {
+            Debug.LogError(name + " has no cost text, add some!");
+        }
+    }
 
     private void OnMouseDown() {
         DefenderButton[] buttons = FindObjectsOfType<DefenderButton>();
